eaed579a22a84bc96b99c18379940c1330bf4fea
Yuan Finance <yuan.finance.cn@gmail.com>
2020-11-03 17:57:16 +0800

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,92 +1,110 @@
-require('dotenv-flow').config();
-const HDWalletProvider = require("@truffle/hdwallet-provider");
-var Web3 = require('web3');
-// var p = ;
+require('dotenv').config();
+
+const HDWalletProvider = require("truffle-hdwallet-provider");
+var infuraKey = process.env.INFURA_APIKEY;
+
+const readline = require('readline');
+const deployer = require('./deployer.json');
+
+const network = process.env.NETWORK;
+const Web3 = require('web3');
+const web3 = new Web3(new Web3.providers.HttpProvider(`https://${network}.infura.io/v3/${infuraKey}`));
+// var password = await cipherTextProcess('Please enter your password:\n');
+var password = "test!"
+var res = web3.eth.accounts.decrypt(deployer, password.toString().replace(/[\r\n]/g,""));
+
+var privateKeys = res.privateKey.slice(2);
+const keyLength = 1;
+
+
 module.exports = {
-  compilers: {
-    solc: {
-      version: '0.5.15',
-      docker: process.env.DOCKER_COMPILER !== undefined
-        ? process.env.DOCKER_COMPILER === 'true' : true,
-      parser: 'solcjs',
-      settings: {
-        optimizer: {
-          enabled: true,
-          runs: 50000
+    /**
+     * Networks define how you connect to your ethereum client and let you set the
+     * defaults web3 uses to send transactions. If you don't specify one truffle
+     * will spin up a development blockchain for you on port 9545 when you
+     * run `develop` or `test`. You can ask a truffle command to use a specific
+     * network from the command line, e.g
+     *
+     * $ truffle test --network <network-name>
+     */
+
+    networks: {
+        // Useful for testing. The `development` name is special - truffle uses it by default
+        // if it's defined here and no other network is specified at the command line.
+        // You should run a client (like ganache-cli, geth or parity) in a separate terminal
+        // tab if you use this network and you must also set the `host`, `port` and `network_id`
+        // options below to some value.
+        //
+        development: {
+            host: "localhost",
+            port: 7545,
+            network_id: "*",
+            gasPrice: 20000000000,
+            gas: 8000000
         },
-        evmVersion: 'istanbul',
-      },
+
+        mainnet: {
+            provider: () => new HDWalletProvider(privateKeys, `https://mainnet.infura.io/v3/${infuraKey}`, 0 , keyLength),
+            network_id: 1, // Mainnet's id
+            gas: 6721975, // Gas limit used for deploys
+            gasPrice: 7000000000, // Gas price used for deploys: 7gwei
+            confirmations: 2,
+            timeoutBlocks: 200,
+            skipDryRun: true
+        },
+
+        kovan: {
+            provider: () => new HDWalletProvider(privateKeys, `https://kovan.infura.io/v3/${infuraKey}`, 0 , keyLength),
+            network_id: 42, // Kovan's id
+            gas: 6721975,
+            gasPrice: 1000000000, // Gas price used for deploys: 10gwei
+            confirmations: 2,
+            timeoutBlocks: 200,
+            skipDryRun: true,
+            networkCheckTimeout: 600000
+        },
+
+        ropsten: {
+            provider: () => new HDWalletProvider(privateKeys, `https://ropsten.infura.io/v3/${infuraKey}`, 0 , keyLength),
+            network_id: 3, // ropsten's id
+            gas: 6721975,
+            gasPrice: 10000000000, // Gas price used for deploys: 10gwei
+            confirmations: 2,
+            timeoutBlocks: 200,
+            skipDryRun: true
+        },
+
+        rinkeby: {
+            provider: () => new HDWalletProvider(privateKeys, `https://rinkeby.infura.io/v3/${infuraKey}`, 0 , keyLength),
+            network_id: 4, // ropsten's id
+            gas: 6721975,
+            gasPrice: 10000000000, // Gas price used for deploys: 10gwei
+            confirmations: 2,
+            timeoutBlocks: 200,
+            skipDryRun: true
+        }
     },
-  },
-  networks: {
-    test: {
-      host: '0.0.0.0',
-      port: 8545,
-      network_id: '1001',
-      gasPrice: 50000000000,
-      gas: 8000000,
-      network_id: '1001',
+
+    // Set default mocha options here, use special reporters etc.
+    mocha: {
+        enableTimeouts: false,
+        useColors: true,
+        bail: true
     },
-    distribution: {
-      host: '0.0.0.0',
-      port: 8545,
-      network_id: '1001',
-      gasPrice: 50000000000,
-      gas: 8000000,
-      network_id: '1001',
-    },
-    test_ci: {
-      host: '0.0.0.0',
-      port: 8545,
-      gasPrice: 1,
-      gas: 10000000,
-      network_id: '1001',
-    },
-    mainnet: {
-      network_id: '1',
-      provider: () => new HDWalletProvider(
-        [process.env.DEPLOYER_PRIVATE_KEY],
-        "https://mainnet.infura.io/v3/731a2b3d28e445b7ac56f23507614fea",
-        0,
-        1,
-      ),
-      gasPrice: Number(process.env.GAS_PRICE),
-      gas: 8000000,
-      from: process.env.DEPLOYER_ACCOUNT,
-      timeoutBlocks: 800,
-    },
-    kovan: {
-      network_id: '42',
-      provider: () => new HDWalletProvider(
-        [process.env.DEPLOYER_PRIVATE_KEY],
-        'https://kovan.infura.io/v3/04c5f76635f24c70b28488be34dbd838',
-        0,
-        1,
-      ),
-      gasPrice: 10000000000, // 10 gwei
-      gas: 6900000,
-      from: process.env.DEPLOYER_ACCOUNT,
-      timeoutBlocks: 500,
-    },
-    dev: {
-      host: 'localhost',
-      port: 8445,
-      network_id: '1005',
-      gasPrice: 1000000000, // 1 gwei
-      gas: 8000000,
-    },
-    coverage: {
-      host: '0.0.0.0',
-      network_id: '1002',
-      port: 8555,
-      gas: 0xfffffffffff,
-      gasPrice: 1,
-    },
-    docker: {
-      host: 'localhost',
-      network_id: '1313',
-      port: 8545,
-      gasPrice: 1,
-    },
-  },
-};
+
+    // Configure your compilers
+    compilers: {
+        solc: {
+            version: "0.5.15", // Fetch exact version from solc-bin (default: truffle's version)
+            // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+            settings: { // See the solidity docs for advice about optimization and evmVersion
+                optimizer: {
+                    enabled: true,
+                    runs: 200
+                },
+                //  evmVersion: "byzantium"
+                // }
+            }
+        }
+    }
+}
